95b1a4dd687866bc2333a75dc67ee97a8d61f400
mstrobeck-png <69990559+mstrobeck-png@users.noreply.github.com>
2020-08-28 23:39:52 -0400

    Add files via upload

---
+++ PE3_Functions.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "function.h"
+void PrintName();
+using namespace std;
+
+int main()
+{
+    PrintName();
+}
+
+void PrintName()
+{
+    cout << "MacKenzie Strobeck" << endl;
+}
